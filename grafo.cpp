c547e4b766afbf98d3b742def32e8d71b90f2095
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2019-08-25 16:36:36 -0300

    Now you can write the output in a external file

--- grafo.cpp
+++ grafo.cpp
@@ -6,9 +6,12 @@ int grau[MAXN];
 bool visited[MAXN];
 int nivel[MAXN];
 int pai[MAXN];
+
 using namespace std;
 
 void BFSMatriz(int start){
+  ofstream bfsFile;
+  bfsFile.open("bfsFile.txt");
   //Define todos os visitados como 0;
   memset(visited,false,MAXN);
   //Define todos os níveis como -1;
@@ -23,7 +26,7 @@ void BFSMatriz(int start){
   fila.push(start);
   //Define o Nivel do start como 0
   nivel[start]=0;
-  cout<< "Vértice: "<< start <<", Nível: "<< nivel[start]<<", Pai: "<< pai[start]<<endl;
+  bfsFile<<"Vértice: "<< start <<", Nível: "<< nivel[start]<<", Pai: "<< pai[start]<<endl;
   //Enquanto a fila não estiver vazia
   while(!fila.empty()){
     //Tira o primeiro elemento da fila
@@ -37,14 +40,17 @@ void BFSMatriz(int start){
         visited[i] = true;
         nivel[i] = nivel[v]+1;
         pai[i] = v;
-        cout<< "Vértice: "<< i <<", Nível: "<< nivel[i]<<", Pai: "<< pai[i]<<endl;
+        bfsFile<< "Vértice: "<< i <<", Nível: "<< nivel[i]<<", Pai: "<< pai[i]<<endl;
         fila.push(i);
       }
     }
   }
+  bfsFile.close();
 }
 
 void DFSMatriz(int start){
+  ofstream dfsFile;
+  dfsFile.open("dfsFile.txt");
   //Define todos os vértices como não visitados
   memset(visited,false,MAXN);
   //Define todos os níveis como -1;
@@ -57,7 +63,7 @@ void DFSMatriz(int start){
   pilha.push(start);
   //Define o Nivel do start como 0
   nivel[start]=0;
-  cout<< "Vértice: "<< start <<", Nível: "<< nivel[start]<<", Pai: "<< pai[start]<<endl;
+  dfsFile<< "Vértice: "<< start <<", Nível: "<< nivel[start]<<", Pai: "<< pai[start]<<endl;
   //Enquanto a pilha não estiver vazia
   while(!pilha.empty()){
     //Retira o elemento do topo da pilha
@@ -74,7 +80,7 @@ void DFSMatriz(int start){
           if (pai[v]==-1){
             pai[v] = u;
             nivel[v] = nivel[u]+1;
-            cout<< "Vértice: "<< v <<", Nível: "<< nivel[v]<<", Pai: "<< pai[v]<<endl;
+            dfsFile<< "Vértice: "<< v <<", Nível: "<< nivel[v]<<", Pai: "<< pai[v]<<endl;
           }
           //Adiciona a vizinhança a pilha
           pilha.push(v);
@@ -82,6 +88,7 @@ void DFSMatriz(int start){
       }
     }
   }
+  dfsFile.close();
 }
 
 int grauMedio(int list[]){
@@ -103,18 +110,17 @@ int medianaGrau(int list[], int numVertices){
 
 int main(){
   int numVertices;
-  int numArestas=0, soma=0;
+  int numArestas=0;
   int vertex1, vertex2;
-  ifstream graphFile("grafo.txt");
+  ifstream graphTexto("grafo.txt");
 
   //Esse é o próprio número de vértices
-  graphFile >> numVertices;
-  cout<<"Esse é o número de vértices: "<<numVertices<<endl;
+  graphTexto >> numVertices;
   //Zera a Matriz de Adjacência
   memset(adjMatriz,false,numVertices);
 
   //Preenche a Matriz de Adjacência
-  while(graphFile>>vertex1>>vertex2){
+  while(graphTexto>>vertex1>>vertex2){
     //Insere o par (vértice1, vértice2) na matriz
     adjMatriz[vertex1][vertex2]=true;
     adjMatriz[vertex2][vertex1]=true;
@@ -128,17 +134,24 @@ int main(){
   //Ordena em O(n.log(n)) o vetor de graus.
   sort(grau, grau+numVertices+1);
 
-  cout<<"Esse é o número de arestas: "<<numArestas<<endl;
+  ofstream graphFile;
+  graphFile.open("graphFile.txt");
+
+  graphFile<< "Esse é o número de vértices: "<<numVertices<<endl;
+  graphFile<< "Esse é o número de arestas: "<<numArestas<<endl;
   //Após ordenado, o grau máximo é o ultimo elemento da lista
-  cout<<"Esse é o grau máximo: "<<grau[numVertices]<<endl;
+  graphFile<< "Esse é o grau máximo: "<<grau[numVertices]<<endl;
   //O grau mínimo será o primeiro elemento +1 (o primeiro será sempre 0)
-  cout<<"Esse é o grau mínimo: "<<grau[1]<<endl;
+  graphFile<< "Esse é o grau mínimo: "<<grau[1]<<endl;
   //O grau médio será o somatorio de cada grau dividido pelo numero de graus
-  cout<<"Esse é o grau médio: "<<grauMedio(grau)<<endl;
-  cout<<"Essa é a mediana do grau: "<<medianaGrau(grau, numVertices)<<endl;
+  graphFile<< "Esse é o grau médio: "<<grauMedio(grau)<<endl;
+  graphFile<<"Essa é a mediana do grau: "<<medianaGrau(grau, numVertices)<<endl;
+
+  graphFile.close();
 
   clock_t start = clock();
-  DFSMatriz(1);
+  BFSMatriz(1);
   clock_t end = clock();
   cout<< (double)(end-start)/CLOCKS_PER_SEC<<endl;
 }
+
