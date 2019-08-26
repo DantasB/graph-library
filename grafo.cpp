cc0d6accea87ffbc4ff6035e01703f34f33e1230
Bruno Dantas de Paiva <bruno.d.paiva@hotmail.com>
2019-08-26 20:24:23 -0300

    Now it's possible to create a matrix from input

--- grafo.cpp
+++ grafo.cpp
@@ -1,27 +1,23 @@
 #include <bits/stdc++.h>
 
-#define MAXN 32386
-bool adjMatriz[MAXN][MAXN];
-int grau[MAXN];
-bool visited[MAXN];
-int nivel[MAXN];
-int pai[MAXN];
-
 using namespace std;
 
-void BFSMatriz(int start){
+void bfsMatriz(int start, bool **adjMatriz, size_t numVertices){
   ofstream bfsFile;
   bfsFile.open("bfsFile.txt");
-  //Define todos os visitados como 0;
-  memset(visited,false,MAXN);
-  //Define todos os níveis como -1;
-  memset(nivel,-1,MAXN);
-  //Define todos os pais como -1;
-  memset(pai,-1,MAXN);
+  //Cria um vetor de niveis
+  int *nivel;
+  nivel = new int[numVertices+1];
+  //Cria um vetor de pais
+  int *pai;
+  pai = new int[numVertices+1];
+  //Define todos os níveis e pais como -1
+  for(int i=0;i<(int)numVertices+1;i++){
+    nivel[i]= -1;
+    pai[i]= -1;
+  }
   //Cria uma fila
   queue<int> fila;
-  //Visita o elemento start
-  visited[start]=true;
   //Adiciona o start a fila
   fila.push(start);
   //Define o Nivel do start como 0
@@ -33,11 +29,10 @@ void BFSMatriz(int start){
     int v = fila.front();
     fila.pop();
     //Para todos os vizinhos da fila
-    for(int i=0;i<MAXN;i++){
+    for(int i=1;i<(int)numVertices+1;i++){
       //Se o vizinho não for visitado
-      if(adjMatriz[v][i] != 0 && (!visited[i])){
+      if(adjMatriz[v][i] != 0 && (nivel[i] == -1)){
         //visita o vizinho e adiciona ele na fila
-        visited[i] = true;
         nivel[i] = nivel[v]+1;
         pai[i] = v;
         bfsFile<< "Vértice: "<< i <<", Nível: "<< nivel[i]<<", Pai: "<< pai[i]<<endl;
@@ -48,15 +43,24 @@ void BFSMatriz(int start){
   bfsFile.close();
 }
 
-void DFSMatriz(int start){
+void dfsMatriz(int start, bool **adjMatriz, size_t numVertices){
   ofstream dfsFile;
   dfsFile.open("dfsFile.txt");
-  //Define todos os vértices como não visitados
-  memset(visited,false,MAXN);
-  //Define todos os níveis como -1;
-  memset(nivel,-1,MAXN);
-  //Define todos os pais como -1;
-  memset(pai,-1,MAXN);
+  //Cria um vetor de niveis
+  int *nivel;
+  nivel = new int[numVertices+1];
+  //Cria um vetor de pais
+  int *pai;
+  pai = new int[numVertices+1];
+  //Cria um vetor de visitados
+  bool *visited;
+  visited = new bool[numVertices+1];
+  //Define todos os níveis e pais como -1 e visitados como 0
+  for(int i=0;i<(int)numVertices+1;i++){
+    nivel[i]= -1;
+    pai[i]= -1;
+    visited[i] = 0;
+  }
   //Cria uma pilha
   stack <int> pilha;
   //Adiciona start a pilha;
@@ -74,7 +78,7 @@ void DFSMatriz(int start){
       //Visita o topo da pilha
       visited[u] = true;
       //Para todas as vizinhanças do topo da pilha
-      for(int v=0; v<MAXN; v++){
+      for(int v=0; v<(int)numVertices+1; v++){
         if(adjMatriz[u][v]!=0){
           //Se o vértice ainda não tem um pai
           if (pai[v]==-1){
@@ -91,31 +95,30 @@ void DFSMatriz(int start){
   dfsFile.close();
 }
 
-int grauMedio(int list[]){
+int grauMedio(int *grau, size_t numVertices){
   int soma=0;
-  for(int i=0; i<MAXN;i++){
+  for(int i=1; i<(int)numVertices+1;i++){
     soma+= grau[i];
   }
-  return soma/(MAXN-1);
+  return soma/(numVertices-1);
 }
 
-int medianaGrau(int list[], int numVertices){
+int medianaGrau(int *grau, size_t numVertices){
   if (numVertices%2==0){
-    return (grau[(numVertices/2)]+ grau[(numVertices/2)+1])/2;
+    return (grau[((int)numVertices/2)]+ grau[((int)numVertices/2)+1])/2;
   }
   else{
-    return grau[(numVertices/2)+1];
+    return grau[((int)numVertices/2)+1];
   }
 }
 
-int distancia(int v1, int v2){
-  //Define todos os visitados como -1;
-  memset(visited,false,MAXN);
+int distanciaMatriz(int v1, int v2, bool **adjMatriz, size_t numVertices){
+  //Cria um vetor distâncias
+  int *path;
+  path = new int[numVertices+1];
   //Define todas as distâncias como -1;
-  memset(path,-1,MAXN);
+  memset(path,-1,numVertices);
   queue<int> fila;
-  //Visita o elemento start
-  visited[v1]=true;
   //Adiciona o start a fila
   fila.push(v1);
   //Define a distância até o start em 0
@@ -126,7 +129,7 @@ int distancia(int v1, int v2){
     int v = fila.front();
     fila.pop();
     //Para todos os vizinhos da fila
-    for(int i=0;i<MAXN;i++){
+    for(int i=1;i<(int)numVertices;i++){
       //Se o vizinho tiver uma distância não definida
       if(adjMatriz[v][i] != 0 && path[i]==-1){
         //Altera a distância e adiciona ele na fila
@@ -138,61 +141,78 @@ int distancia(int v1, int v2){
   return path[v2];
 }
 
-void bfs(int start, int constante){
-  //Define todos os visitados como 0;
-  memset(visited,false,MAXN);
+//int diametroMatriz(){}
+
+void bfs(int start, bool **adjMatriz, size_t numVertices, int *conexas, int constante){
   //Cria uma fila
   queue<int> fila;
   //Visita o elemento start
-  visited[start]=true;
+  conexas[start]=constante;
   //Adiciona o start a fila
   fila.push(start);
-  cout<<start<<" ";
   //Enquanto a fila não estiver vazia
   while(!fila.empty()){
     //Tira o primeiro elemento da fila
     int v = fila.front();
     fila.pop();
     //Para todos os vizinhos da fila
-    for(int i=1;i<MAXN;i++){
+    for(int i=1;i<(int)numVertices+1;i++){
       //Se o vizinho não for visitado
-      if(adjMatriz[v][i] != 0 && (!visited[i])){
+      if(adjMatriz[v][i] != 0 && (conexas[i]==-1)){
         //visita o vizinho e adiciona ele na fila
-        visited[i] = true;
         conexas[i]=constante;
-        cout<<i<<" ";
         fila.push(i);
       }
     }
   }
-  cout<<endl;
 }
 
-int numeroCCMatriz(bool vertices[][MAXN]){
-  memset(conexas, 0, MAXN);
+int numeroCCMatriz(bool **adjMatriz, size_t numVertices){
+  //Cria um vetor de componentes conexas
+  int *conexas;
+  conexas = new int[numVertices+1];
+  //Define todos as conexas como -1
+  for(int i=0;i<(int)numVertices+1;i++){
+  conexas[i]=-1;
+}
   int constante = 0;
   int resultado = 0;
-  for(int i=1;i<MAXN;i++){
-    if (!visited[i]){
+  for(int i=1;i<(int)numVertices+1;i++){
+    if (conexas[i]==-1){
       resultado++;
-      bfs(i,constante);
+      bfs(i, adjMatriz, numVertices, conexas, constante);
       constante++;
     }
   }
   return resultado;
 }
 
+
 int main(){
   int numVertices;
+  bool **adjMatriz;
+  int *grau;
   int numArestas=0;
   int vertex1, vertex2;
   ifstream graphTexto("grafo.txt");
 
   //Esse é o próprio número de vértices
   graphTexto >> numVertices;
-  //Zera a Matriz de Adjacência
-  memset(adjMatriz,false,numVertices);
 
+  //Cria a Matriz de Adjacência
+  adjMatriz = new bool*[numVertices+1];
+  for (int i = 0; i < numVertices+1; i++){
+        adjMatriz[i] = new bool[numVertices+1];
+  }
+
+  //Zera a Matriz de Adjacência
+  for (int i=0;i < numVertices+1; i++){
+    for(int j=0;j< numVertices+1;j++){
+        adjMatriz[i][j] = false;
+    }
+  }
+  //Cria um vetor de graus
+  grau = new int[numVertices+1]();
   //Preenche a Matriz de Adjacência
   while(graphTexto>>vertex1>>vertex2){
     //Insere o par (vértice1, vértice2) na matriz
@@ -218,14 +238,15 @@ int main(){
   //O grau mínimo será o primeiro elemento +1 (o primeiro será sempre 0)
   graphFile<< "Esse é o grau mínimo: "<<grau[1]<<endl;
   //O grau médio será o somatorio de cada grau dividido pelo numero de graus
-  graphFile<< "Esse é o grau médio: "<<grauMedio(grau)<<endl;
+  graphFile<< "Esse é o grau médio: "<<grauMedio(grau, numVertices)<<endl;
   graphFile<<"Essa é a mediana do grau: "<<medianaGrau(grau, numVertices)<<endl;
 
   graphFile.close();
-
+  /*
   clock_t start = clock();
-  BFSMatriz(1);
+  cout<<numeroCCMatriz(adjMatriz, numVertices)<<endl;
   clock_t end = clock();
   cout<< (double)(end-start)/CLOCKS_PER_SEC<<endl;
+  */
 }
 
